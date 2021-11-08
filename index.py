e69f4a20671bbc354cd3fef7cb0142968bcd2cfe
Armando Roca <consultor.cfdi@gmail.com>
2021-11-08 12:44:29 -0600

    paginas accesorias creadas y bio

--- index.py
+++ index.py
@@ -10,6 +10,20 @@ def home():
 def about():
     return render_template('about.html')
 
+@app.route('/prods')
+def prods():
+    return render_template('productos.html')
+
+@app.route('/servs')
+def servicios():
+    return render_template('servicios.html')
+
+@app.route('/bio')
+def c_v():
+    return render_template('bio.html')
+
+
+
 if __name__=='__main__':
     app.run(debug=True)
 
